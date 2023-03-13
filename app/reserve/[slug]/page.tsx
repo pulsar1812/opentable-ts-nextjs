b599675cf30a110045d2b4ba440925e98cda4279
pulsar1812 <cluster000@gmail.com>
2023-03-13 23:37:06 +0800

    Replace anchor links with Links

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -8,10 +8,9 @@ export default function Reserve({}: Props) {
       <main className='max-w-screen-2xl m-auto bg-white'>
         {/* NAVBAR */}
         <nav className='bg-white p-2 flex justify-between'>
-          <a href='' className='font-bold text-gray-700 text-2xl'>
-            {' '}
-            OpenTable{' '}
-          </a>
+          <Link href='/' className='font-bold text-gray-700 text-2xl'>
+            OpenTable
+          </Link>
           <div>
             <div className='flex'>
               <button className='bg-blue-400 text-white border p-1 px-4 rounded mr-3'>
