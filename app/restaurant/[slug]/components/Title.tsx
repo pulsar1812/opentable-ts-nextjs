c78a351e41eda6e4d584359726a81e1e6de932dd
pulsar1812 <cluster000@gmail.com>
2023-03-17 23:42:49 +0800

    Refactor single restaurant page

---
+++ app/restaurant/[slug]/components/Title.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Title({}: Props) {
+  return (
+    <div className='mt-4 border-b pb-6'>
+      <h1 className='font-bold text-6xl'>Milesstone Grill</h1>
+    </div>
+  )
+}
