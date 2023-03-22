0ba3a499029a57c5a6abfbcbb1f1b58541e2cee5
pulsar1812 <cluster000@gmail.com>
2023-03-22 23:40:43 +0800

    Refactor more

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,6 +1,7 @@
-import Link from 'next/link'
 import Navbar from '../../../components/Navbar'
 import Header from '../components/Header'
+import Menu from '../components/Menu'
+import RestaurantNavbar from '../components/RestaurantNavbar'
 
 type Props = {}
 
@@ -10,41 +11,12 @@ export default function RestaurantMenu({}: Props) {
       <main className='max-w-screen-2xl m-auto bg-white'>
         <Navbar />
         <Header />
-        {/* DESCRIPTION PORTION */}
         <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
           <div className='bg-white w-[100%] rounded p-3 shadow'>
-            {/* RESAURANT NAVBAR */}
-            <nav className='flex text-reg border-b pb-2'>
-              <Link href='/restaurant/milestones-grill' className='mr-7'>
-                Overview
-              </Link>
-              <Link href='/restaurant/milestones-grill/menu' className='mr-7'>
-                Menu
-              </Link>
-            </nav>
-            {/* RESAURANT NAVBAR */} {/* MENU */}
-            <main className='bg-white mt-5'>
-              <div>
-                <div className='mt-4 pb-1 mb-1'>
-                  <h1 className='font-bold text-4xl'>Menu</h1>
-                </div>
-                <div className='flex flex-wrap justify-between'>
-                  {/* MENU CARD */}
-                  <div className=' border rounded p-3 w-[49%] mb-3'>
-                    <h3 className='font-bold text-lg'>Surf and Turf</h3>
-                    <p className='font-light mt-1 text-sm'>
-                      A well done steak with lobster and rice
-                    </p>
-                    <p className='mt-7'>$80.00</p>
-                  </div>
-                  {/* MENU CARD */}
-                </div>
-              </div>
-            </main>
-            {/* MENU */}
+            <RestaurantNavbar />
+            <Menu />
           </div>
         </div>
-        {/* DESCRIPTION PORTION */}
       </main>
     </main>
   )
