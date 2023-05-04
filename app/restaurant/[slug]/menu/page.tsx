f1ff93a96222dfa238fda59910dadef15a342d91
pulsar1812 <cluster000@gmail.com>
2023-05-04 23:48:21 +0800

    Add layout and head for search

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,5 +1,3 @@
-import Navbar from '../../../components/Navbar'
-import Header from '../components/Header'
 import Menu from '../components/Menu'
 import RestaurantNavbar from '../components/RestaurantNavbar'
 
@@ -7,17 +5,11 @@ type Props = {}
 
 export default function RestaurantMenu({}: Props) {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <Navbar />
-        <Header />
-        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-          <div className='bg-white w-[100%] rounded p-3 shadow'>
-            <RestaurantNavbar />
-            <Menu />
-          </div>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className='bg-white w-[100%] rounded p-3 shadow'>
+        <RestaurantNavbar />
+        <Menu />
+      </div>
+    </>
   )
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 
 import Description from './components/Description'
-import Header from './components/Header'
 import Images from './components/Images'
 import Rating from './components/Rating'
 import ReservationCard from './components/ReservationCard'
@@ -14,19 +13,16 @@ type Props = {}
 export default function RestaurantDetails({}: Props) {
   return (
     <>
-      <Header />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        <div className='bg-white w-[70%] rounded p-3 shadow'>
-          <RestaurantNavbar />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <div className='w-[27%] relative text-reg'>
-          <ReservationCard />
-        </div>
+      <div className='bg-white w-[70%] rounded p-3 shadow'>
+        <RestaurantNavbar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className='w-[27%] relative text-reg'>
+        <ReservationCard />
       </div>
     </>
   )
