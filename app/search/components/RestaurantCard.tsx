7ed1f524d515d2e16e94dd49a4ccf13bc22b8659
pulsar1812 <cluster000@gmail.com>
2023-05-16 23:49:18 +0800

    Add search restaurants by city

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,31 +1,41 @@
-import React from 'react'
 import Link from 'next/link'
+import { Cuisine, PRICE, Location } from '@prisma/client'
 
-type Props = {}
+interface Restaurant {
+  id: number
+  name: string
+  main_image: string
+  slug: string
+  price: PRICE
+  cuisine: Cuisine
+  location: Location
+}
 
-export default function RestaurantCard({}: Props) {
+export default function RestaurantCard({
+  restaurant,
+}: {
+  restaurant: Restaurant
+}) {
+  const { name, main_image, slug, price, cuisine, location } = restaurant
+
   return (
-    <div className='border-b flex pb-5'>
-      <img
-        src='https://images.otstatic.com/prod1/49153814/2/medium.jpg'
-        alt=''
-        className='w-44 rounded'
-      />
+    <div className='border-b flex pb-5 ml-4'>
+      <img src={main_image} alt='' className='w-44 rounded' />
       <div className='pl-5'>
-        <h2 className='text-3xl'>Aiāna Restaurant Collective</h2>
+        <h2 className='text-3xl'>{name}</h2>
         <div className='flex items-start'>
           <div className='flex mb-2'>*****</div>
           <p className='ml-2 text-sm'>Awesome</p>
         </div>
         <div className='mb-9'>
           <div className='font-light flex text-reg'>
-            <p className='mr-4'>$$$</p>
-            <p className='mr-4'>Mexican</p>
-            <p className='mr-4'>Ottawa</p>
+            <p className='mr-4'>{price}</p>
+            <p className='mr-4'>{cuisine.name}</p>
+            <p className='mr-4'>{location.name}</p>
           </div>
         </div>
         <div className='text-red-600'>
-          <Link href='/restaurant/milestones-grill'>View more information</Link>
+          <Link href={`/restaurant/${slug}`}>View more information</Link>
         </div>
       </div>
     </div>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,19 +1,57 @@
-import Link from 'next/link'
-
+import { PrismaClient } from '@prisma/client'
 import RestaurantCard from './components/RestaurantCard'
 import SearchHeader from './components/SearchHeader'
 import SearchSidebar from './components/SearchSidebar'
 
-type Props = {}
+const prisma = new PrismaClient()
 
-export default function Search({}: Props) {
+const fetchRestaurantsByCity = async (city: string | undefined) => {
+  if (!city) return await prisma.restaurant.findMany()
+
+  const restaurants = await prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      slug: true,
+      price: true,
+      cuisine: true,
+      location: true,
+    },
+  })
+
+  return restaurants
+}
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city: string }
+}) {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city)
+
   return (
     <>
       <SearchHeader />
       <div className='flex py-4 m-auto w-2/3 justify-between items-start'>
         <SearchSidebar />
         <div className='w-5/6'>
-          <RestaurantCard />
+          {restaurants.length ? (
+            <>
+              {restaurants.map((restaurant) => (
+                <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+              ))}
+            </>
+          ) : (
+            <p>No restaurants found</p>
+          )}
         </div>
       </div>
     </>
